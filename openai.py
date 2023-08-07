ac94a1ff2a17597159dad3d894355337c35d00e4
kushwap1 <pankaj.kushwaha@yahoo.com>
2023-08-08 02:30:49 +0530

    first

---
+++ openai.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+
+import os 
+import sys
+import openai
+
+def parse_log_file(log_file):
+	with open(log_file, 'r') as f:
+		log_file_contents = f.read()
+	return log_file_contents
+
+def generate_summary(log_file_contents):
+	openai.api_key = os.environ.get('OPENAI_SECRET_KEY')
+	response = openai.Completion.create(
+		engine="text-davinci-003",
+		prompt=log_file_contents,
+		temperature=0.9,
+		max_tokens=150,
+		top_p=1,
+		frequency_penality=0
+		presence_penalty=0)
+
+	return response
+
+log_file_contents = parse_log_file(sys.argv[1])
+
+response = generate_summary(log_file_contents)
+
+print(response['choices'][0][''text])
