a821462c73de292f325a5bc479b3801638583b13
kushwap1 <pankaj.kushwaha@yahoo.com>
2023-08-09 01:57:52 +0530

    first

--- scriptopenai.py
+++ scriptopenai.py
@@ -26,3 +26,8 @@ log_file_contents = parse_log_file(sys.argv[1])
 response = generate_summary(log_file_contents)
 
 print(response['choices'][0]['text'])
+
+with open('summary.txt', 'w') as f:
+	f.write(response['choices'][0]['text'])
+	f.close()
+
