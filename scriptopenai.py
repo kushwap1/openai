5dd507c16b8c40662a90458dbaa10fccb7f9b424
kushwap1 <pankaj.kushwaha@yahoo.com>
2023-08-09 01:19:17 +0530

    first

--- scriptopenai.py
+++ scriptopenai.py
@@ -12,7 +12,7 @@ def parse_log_file(log_file):
 def generate_summary(log_file_contents):
 	openai.api_key = os.environ.get('OPENAI_SECRET_KEY')
 	response = openai.Completion.create(
-		engine="text-davinci-003",
+		model="text-davinci-003",
 		prompt=log_file_contents,
 		temperature=0.7,
 		max_tokens=1024,
